233f8fbcbd3f07ebfb49993fd81ba2add66ffcf2
Rachel Isaac <rachelisaac995@gmail.com>
2022-07-04 22:00:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const smallImage = document.querySelectorAll(".sm-img");
-const largeImage = document.querySelector(".upper-image");
+const firstLargeImage = document.querySelector(".upper-image");
+const secondLargeImage = document.querySelector(".second-large-image");
+const thirdLargeImage = document.querySelector(".third-large-image");
+const fourthLargeImage = document.querySelector(".fourth-large-image");
 const lowerDivImage = document.querySelector(".lower-image");
 const upperDivImage = document.querySelector(".upper-image-cont");
 
@@ -9,6 +12,17 @@ for (let i = 0; i < smallImage.length; i++) {
   smallImage[i].addEventListener("click", changeImage);
   function changeImage() {
     const data = this.dataset.value;
+    if (data === "1") {
+      upperDivImage.innerHTML = '<img src="./images/image-product-1.jpg" />';
+    } else if (data === "2") {
+      upperDivImage.innerHTML = '<img src="./images/image-product-2.jpg" />';
+    } else if (data === "3") {
+      upperDivImage.innerHTML = '<img src="./images/image-product-3.jpg" />';
+    } else if (data === "4") {
+      upperDivImage.innerHTML = '<img src="./images/image-product-4.jpg" />';
+    } else {
+      upperDivImage.innerHTML = '<img src="./images/image-product-1.jpg" />';
+    }
   }
 }
 
